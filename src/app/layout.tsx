93f133f9f832b68b70c69b65a124fe2949ca3c18
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-08 11:47:04 +0530

    Styling Hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { bowlby, dmMono } from './fonts';
+import Header from '@/components/Header';
+import { SVGFilters } from '@/components/SVGFilters';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,8 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bowlby.variable} ${dmMono.variable} antialiased font-mono font-medium text-zinc-800`}>
-        {children}
+      <body
+        className={`${bowlby.variable} ${dmMono.variable} antialiased font-mono font-medium text-zinc-800`}
+      >
+        <main>
+          <Header />
+          {children}
+        </main>
+        <SVGFilters />
       </body>
     </html>
   );
