641b0ae04cb9f02272e9749263ad5b4f01766193
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-11 15:01:41 +0530

    Three: Updating camera angle and Field of View

--- src/slices/HomepageHero/InteractiveSkateboard.tsx
+++ src/slices/HomepageHero/InteractiveSkateboard.tsx
@@ -7,7 +7,10 @@ type Props = {};
 function InteractiveSkateboard({}: Props) {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
-      <Canvas className="min-h-[60rem] w-full">
+      <Canvas
+        className="min-h-[60rem] w-full"
+        camera={{ position: [1.5, 1, 1.4], fov: 55 }}
+      >
         <Suspense fallback="">
           <Scene />
         </Suspense>
