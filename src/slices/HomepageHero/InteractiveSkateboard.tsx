2206039f14578c170ca4305f1df47f0c6da6e331
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-11 14:54:53 +0530

    Added basic shape of three js

--- src/slices/HomepageHero/InteractiveSkateboard.tsx
+++ src/slices/HomepageHero/InteractiveSkateboard.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+'use client';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense } from 'react';
 
 type Props = {};
 
 function InteractiveSkateboard({}: Props) {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
-      InteractiveSkateboard
+      <Canvas className="min-h-[60rem] w-full">
+        <Suspense fallback="">
+          <Scene />
+        </Suspense>
+      </Canvas>
     </div>
+  );
+}
+
+function Scene() {
+  return (
+    <group>
+      <mesh>
+        <meshBasicMaterial />
+        <boxGeometry />
+      </mesh>
+    </group>
   );
 }
 
