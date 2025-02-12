5b9bb32bc3f0e2f5396e9fe5a45957565533d080
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-12 15:07:32 +0530

    Added First Skateboard effect

--- src/slices/HomepageHero/InteractiveSkateboard.tsx
+++ src/slices/HomepageHero/InteractiveSkateboard.tsx
@@ -1,8 +1,10 @@
 'use client';
+import * as THREE from 'three';
 import { SkateboardModel } from '@/components/SkateboardModel';
 import { ContactShadows, Environment, OrbitControls } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import React, { Suspense } from 'react';
+import { Canvas, ThreeEvent } from '@react-three/fiber';
+import React, { Suspense, useRef } from 'react';
+import gsap from 'gsap';
 
 type Props = {
   deckTextureURL: string;
@@ -43,6 +45,56 @@ function Scene({
   truckColor,
   boltColor,
 }: Props) {
+  const containerRef = useRef<THREE.Group>(null);
+
+  function onClick(event: ThreeEvent<MouseEvent>) {
+    event.stopPropagation();
+    const board = containerRef.current;
+
+    if (!board) return;
+
+    const { name } = event.object;
+
+    ollie(board);
+  }
+
+  function ollie(board: THREE.Group) {
+    jumpBoard(board);
+
+    gsap
+      .timeline()
+      .to(board.rotation, {
+        x: -0.6,
+        duration: 0.26,
+        ease: 'none',
+      })
+      .to(board.rotation, {
+        x: 0.4,
+        duration: 0.82,
+        ease: 'power2.in',
+      })
+      .to(board.rotation, {
+        x: 0,
+        duration: 0.26,
+        ease: 'none',
+      });
+  }
+  function jumpBoard(board: THREE.Group) {
+    gsap
+      .timeline()
+      .to(board.position, {
+        y: 0.8,
+        duration: 0.51,
+        ease: 'power2.out',
+        delay: 0.26,
+      })
+      .to(board.position, {
+        y: 0.0,
+        duration: 0.43,
+        ease: 'power2.in',
+        // delay: 0.26,
+      });
+  }
   return (
     <group>
       {/* <pointLight position={[1, 1, 1]} intensity={5} /> */}
@@ -54,15 +106,23 @@ function Scene({
         <meshStandardMaterial />
         <boxGeometry />
       </mesh> */}
-      <SkateboardModel
-        deckTextureURL={deckTextureURL}
-        deckTextureURLs={[deckTextureURL]}
-        wheelTextureURL={wheelTextureURL}
-        wheelTextureURLs={[wheelTextureURL]}
-        truckColor={truckColor}
-        boltColor={boltColor}
-        constantWheelSpin
-      />
+      <group ref={containerRef} position={[-0.25, 0, -0.635]}>
+        <group position={[0, -0.86, 0.635]}>
+          <SkateboardModel
+            deckTextureURL={deckTextureURL}
+            deckTextureURLs={[deckTextureURL]}
+            wheelTextureURL={wheelTextureURL}
+            wheelTextureURLs={[wheelTextureURL]}
+            truckColor={truckColor}
+            boltColor={boltColor}
+            constantWheelSpin
+          />
+          <mesh position={[0, 0.27, 0]} name="middle" onClick={onClick}>
+            <boxGeometry args={[0.6, 0.1, 1.2]} />
+            <meshStandardMaterial visible={true} />
+          </mesh>
+        </group>
+      </group>
       <ContactShadows opacity={0.6} position={[0, -0.08, 0]} />
     </group>
   );
