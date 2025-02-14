e1558c08e374dcd263b0963be221c1c3de297c96
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-14 14:50:45 +0530

    Setting up Skateboard build environment

--- src/app/build/Preview.tsx
+++ src/app/build/Preview.tsx
@@ -1,6 +1,12 @@
 'use client';
 
-import { CameraControls, Environment, Preload } from '@react-three/drei';
+import * as THREE from 'three';
+import {
+  CameraControls,
+  Environment,
+  Preload,
+  useTexture,
+} from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React, { Suspense, useRef } from 'react';
 import { useCustomizerControls } from './context';
@@ -20,6 +26,7 @@ const DEFAULT_ENVIRONMENT_COLOR = '#3B3A3A';
 
 export default function Preview({ wheelTextureUrls, deckTextureUrls }: Props) {
   const cameraControls = useRef<CameraControls>(null);
+  const floorRef = useRef<THREE.Mesh>(null);
   const { selectedBolt, selectedDeck, selectedTruck, selectedWheel } =
     useCustomizerControls();
 
@@ -30,8 +37,19 @@ export default function Preview({ wheelTextureUrls, deckTextureUrls }: Props) {
   const truckColor = selectedTruck?.color ?? DEFAULT_TRUCK_COLOR;
   const boltColor = selectedBolt?.color ?? DEFAULT_BOLT_COLOR;
 
+  function onCameraControlStart() {
+    if (
+      !cameraControls.current ||
+      !floorRef.current ||
+      cameraControls.current.colliderMeshes.length > 0
+    )
+      return;
+
+    cameraControls.current.colliderMeshes = [floorRef.current];
+  }
+
   return (
-    <Canvas>
+    <Canvas camera={{ position: [2.5, 1, 0] }} shadows>
       <Suspense fallback={null}>
         <Environment
           files={'/hdr/warehouse-512.hdr'}
@@ -45,6 +63,14 @@ export default function Preview({ wheelTextureUrls, deckTextureUrls }: Props) {
           intensity={1.6}
         />
 
+        <fog attach="fog" args={[DEFAULT_ENVIRONMENT_COLOR, 3, 10]} />
+        <color attach="background" args={[DEFAULT_ENVIRONMENT_COLOR]} />
+        <StageFloor />
+
+        <mesh rotation={[-Math.PI / 2, 0, 0]} ref={floorRef}>
+          <planeGeometry args={[6, 6]} />
+          <meshBasicMaterial visible={false} />
+        </mesh>
         <SkateboardModel
           wheelTextureURLs={wheelTextureUrls}
           wheelTextureURL={wheelTextureUrl}
@@ -58,9 +84,36 @@ export default function Preview({ wheelTextureUrls, deckTextureUrls }: Props) {
           ref={cameraControls}
           minDistance={0.2}
           maxDistance={4}
+          onStart={onCameraControlStart}
         />
       </Suspense>
       <Preload all />
     </Canvas>
+  );
+}
+
+function StageFloor() {
+  const normalMap = useTexture('/concrete-normal.avif');
+  normalMap.wrapS = THREE.RepeatWrapping;
+  normalMap.wrapT = THREE.RepeatWrapping;
+  normalMap.repeat.set(30, 30);
+  normalMap.anisotropy = 8;
+
+  const material = new THREE.MeshStandardMaterial({
+    roughness: 0.75,
+    color: DEFAULT_ENVIRONMENT_COLOR,
+    normalMap: normalMap,
+  });
+
+  return (
+    <mesh
+      material={material}
+      receiveShadow
+      position={[0, -0.005, 0]}
+      castShadow
+      rotation={[-Math.PI / 2, 0, 0]}
+    >
+      <circleGeometry args={[20, 32]} />
+    </mesh>
   );
 }
