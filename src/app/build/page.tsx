ef9ceb9431e15c922fa9f31c2cd8c5e7ef2b2e2b
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-14 11:38:59 +0530

    Readying basic layout

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -1,9 +1,46 @@
+import { ButtonLink } from '@/components/ButtonLink';
+import { Heading } from '@/components/Heading';
+import { Logo } from '@/components/Logo';
+import Link from 'next/link';
 import React from 'react';
 
 type Props = {};
 
 async function Page({}: Props) {
-  return <div>Page</div>;
+  return (
+    <div className="flex min-h-screen flex-col lg:flex-row">
+      <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
+        <Link href="/" className="absolute left-6 top-6">
+          <Logo className="h-12 text-white" />
+        </Link>
+      </div>
+      <div className="grow bg-texture bg-zinc-900 text-white ~p-4/6 lg:w-96 lg:shrink-0 lg:grow-0">
+        <Heading as="h3" size="sm" className="mb-6 mt-0">
+          Build your board
+        </Heading>
+
+        <Heading as="h4" size="xs">
+          Deck |
+        </Heading>
+
+        <Heading as="h4" size="xs">
+          Wheels |
+        </Heading>
+
+        <Heading as="h4" size="xs">
+          Trucks |
+        </Heading>
+
+        <Heading as="h4" size="xs">
+          Bolts |
+        </Heading>
+
+        <ButtonLink href="/" color="lime" icon="plus">
+          Add to Cart
+        </ButtonLink>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
